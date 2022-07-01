69b28aca2adff087256fa8953b449420b9129099
Peter <Walsh9636@gmail.com>
2022-07-01 13:35:59 -0400

    Setupnode project.

---
+++ src/testfile.ts
@@ -0,0 +1,22 @@
+
+/**
+ * Hey look I'm the Person class!
+ */
+export class Person {
+
+    /**
+     * This is the person class constructor
+     */
+    constructor() {
+    }
+
+    /**
+     * I'm a method that adds 5 to a number!
+     * 
+     * @param num a number
+     * @returns the given number + 5
+     */
+    public doSomething(num: number): number {
+        return num + 5;
+    }
+}
